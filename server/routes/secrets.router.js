6749f8d51d8e1fed06cbd926b3ac2daeaa00a31c
Travis Johnston <travisjohnston4588@icloud.com>
2020-01-09 11:24:14 -0600

    secrets.router.js updated with authentication

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
+const userStrategy = require('../strategies/user.strategy');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
     pool.query('SELECT * FROM "secret";')
         .then(results => res.send(results.rows))
