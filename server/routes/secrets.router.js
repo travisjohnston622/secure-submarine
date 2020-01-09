8995a3d2ccce61a68b2e7a7ad451454081a8df85
Travis Johnston <travisjohnston4588@icloud.com>
2020-01-09 13:47:32 -0600

    secrets.router.js updated again

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -5,9 +5,12 @@ const userStrategy = require('../strategies/user.strategy');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    const userLevel = req.user.clearance_level
+   const queryString = `SELECT * FROM "secret" WHERE ${userLevel} >= "secret".secrecy_level;`;
+    pool.query(queryString)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
